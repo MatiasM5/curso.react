ca91a1fc88f6bb8c13a38a2d22ded246f221e937
Matias <moyamatias01@gmail.com>
2022-03-28 12:11:16 -0300

    nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+    <nav>
+       <ul class="nav justify-content-end">
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Productos</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Nosotros</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+        </li>
+       </ul>
+    </nav>
+
+    );
+}
+
+export default Navbar;
