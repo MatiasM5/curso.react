3ad4954fdd2f7429909e4c8d008482d00a84dd36
Matias <moyamatias01@gmail.com>
2022-03-31 14:58:36 -0300

    itemList

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+const ItemListContainer = () => {
+    return(
+        <div>
+            <h4>
+                <p> App de React </p>
+            </h4>
+        </div>
+    );
+}
+
+export default ItemListContainer;
